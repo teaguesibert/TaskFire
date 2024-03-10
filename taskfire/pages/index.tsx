bccf7fd7d36014da62171fd36d5f0ef41cc36873
teaguesibert <teaguesibert.gmail.com>
2024-03-09 23:44:53 -0500

    Updated the color scheme and styling of index.tsx

--- taskfire/pages/index.tsx
+++ taskfire/pages/index.tsx
@@ -47,7 +47,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-[100svh] bg-slate-500">
+    <div className="flex flex-col justify-center items-center h-[100svh] bg-surface-100">
 <Image
       src="/NotesApp.svg"
       width={65}
@@ -59,14 +59,15 @@ const LoginPage = () => {
       
        <h1 className="text-center text-3xl font-bold mb-8" ><span className={noto.className}>TaskFire</span></h1>
 
-      <form className="bg-white shadow-xl rounded px-6 pt-6 pb-8 mb-4" onSubmit={handleLogin}>
-        <h2 className="text-center text-2xl font-bold mb-8 text-amber-500" ><span className={noto.className}>Login</span></h2>
+      <form className="bg-surface-200 shadow-lg rounded-2xl px-6 pt-6 pb-8 mb-4" onSubmit={handleLogin}>
+        <h2 className="text-center text-2xl font-bold mb-8 text-white" ><span className={noto.className}>Welcome!</span></h2>
         <div className="m-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+          <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
             Username
           </label>
           <input 
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+            className="shadow bg-surface-100 appearance-none border-none rounded w-full py-2 px-3 text-surface-600 placeholder-surface-400
+            leading-tight focus:outline-none focus:shadow-outline" 
             id="username" 
             type="text" 
             placeholder="Username"
@@ -75,11 +76,11 @@ const LoginPage = () => {
           />
         </div>
         <div className="m-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
             Password
           </label>
           <input 
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+            className="shadow bg-surface-100 appearance-none border-none rounded w-full py-2 px-3 text-surface-600 placeholder-surface-400 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
             id="password" 
             type="password" 
             placeholder="*********"
@@ -88,13 +89,13 @@ const LoginPage = () => {
           />
         </div>
         
-       {errorMessage && <p className="text-red-500 text-center mb-2">{errorMessage}</p>}
+       {errorMessage && <p className="text-red-300 text-center mb-2">{errorMessage}</p>}
 
         <div className="flex items-center justify-between">
-          <button className="bg-amber-600 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-            Sign In
+          <button className="bg-primary-200 hover:bg-primary-400 text-surface-300 font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline" type="submit">
+            Login
           </button>
-          <a className="inline-block align-baseline font-bold text-sm text-amber-600 hover:text-amber-700" href="#" onClick={() => router.push('/register')}>
+          <a className="inline-block align-baseline font-bold text-sm text-primary-200 hover:text-primary-400" href="#" onClick={() => router.push('/register')}>
           Create An Account
           </a>
         </div>
